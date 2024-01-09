9550c02fd3b68defd514e572696874dffc19063a
Sara Usevska <sarausevska42@yahoo.com>
2024-01-09 13:29:05 +0100

    first commit\

---
+++ Broj.java
@@ -0,0 +1,32 @@
+package nov;
+public class Broj {
+	
+
+	private int a;
+
+	private int b;
+
+	public int geta() {
+
+	return this.a;
+
+	}
+
+	public void seta(int a) {
+
+	this.a=a;
+
+	}
+
+	public int getb() {
+
+	return this.b;
+
+	}
+
+	public void setb(int b) {
+
+	this.b=b;
+
+	}
+}
---
+++ Zbir.java
@@ -0,0 +1,24 @@
+package nov;
+
+public class Zbir {
+		
+		public static void main(String[]args) {
+
+		Broj b=new Broj();
+
+		b.seta(3);
+
+		b.setb(4);
+
+		int c;
+
+		c=b.geta()+b.getb();
+
+		System.out.println(c);
+
+		}
+}
+
+
+
+
